73ace3c45cc2c244573a6b8ad7d67899a18ce186
JohnnyUtah5551 <johnny79126044093@gmail.com>
2025-12-05 16:33:35 +0300

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -1,1 +1,25 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_TO = os.getenv("EMAIL_TO")  # кому отправлять заявки
+
+
+def send_application_email(text: str, amount: int):
+    msg = MIMEText(f"Поступила заявка на сумму {amount} руб.\n\nТекст:\n{text}")
+    msg["Subject"] = f"Новая заявка с сайта на {amount} руб"
+    msg["From"] = EMAIL_USER
+    msg["To"] = EMAIL_TO
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.send_message(msg)
 
