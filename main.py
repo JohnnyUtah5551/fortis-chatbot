70ad522c51aca94dacad54b34b5ed78b424a1f0b
JohnnyUtah5551 <johnny79126044093@gmail.com>
2025-12-24 14:18:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Подключаем папку static, чтобы отдавать widget.js
 app.mount("/static", StaticFiles(directory="static"), name="static")
